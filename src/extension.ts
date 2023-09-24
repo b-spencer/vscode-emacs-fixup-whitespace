2732016dddb709f76aae130fc81e79c96f6c4af7
Brad Spencer <spencer@jacknife.org>
2023-09-23 23:04:21 -0300

    multi works better but cursor wrong sometimes

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,12 @@ function warn(message: string)
   vscode.window.showInformationMessage(message);
 }
 
+// To make it easy to disable console logging.
+function log(message: string)
+{
+  console.log(message);
+}
+
 // The command itself.
 function fixupWhitespace() 
 {
@@ -18,10 +24,28 @@ function fixupWhitespace()
     return;
   }
 
-  console.log("HELLO");
+  log("Starting");
 
-  // We'll be building a list of edits.
+  // We build a list of edits.
   let edits: { erasure: vscode.Range; replacement: string }[] = [];
+
+  // We build an index of whitespace erasure regions so that we can detect when
+  // multiple cursors are sitting in the same regions.  
+  //
+  // The value in this set is the Position of the start of the erasure.  Note
+  // that if two cursors start in the same whitespace region, then they will
+  // identify the same region, and thus find the same start, so that's all we
+  // need to index.
+  //
+  // When we find a subsequent cursor in the same region, we skip it _and_ end
+  // up removing it from the set of cursors.  After all, it has lost its unique
+  // position by our operation!
+  //
+  // *sigh* TypeScript doesn't allow its Set's comparison operation to be
+  // customized, so we do the simplest thing and build a (*yuck*) string key
+  // that maps 1:1 with each value.
+  //
+  let already = new Set<string>();
 
   // We support multiple cursors.  Iterator over them all.
   for(let selection of editor.selections)
@@ -31,11 +55,11 @@ function fixupWhitespace()
 
     // Flatten the line so we can operate on it simply.
     const line = editor.document.lineAt(lineRange.start).text;
-    console.log(`Found line: ${line}`);
+    log(`Found line: ${line}`);
 
     // Figure out where the cursor is within that isolated line.
     const cursor = selection.active.character - lineRange.start.character;
-    console.log(`Found pos : ` + " ".repeat(cursor) + "█");
+    log(`Found pos : ` + " ".repeat(cursor) + "█");
 
     // Split the line at the cursor.
     const prefix = line.substring(0, cursor);
@@ -47,8 +71,12 @@ function fixupWhitespace()
     //
     // By using trimEnd(), we're expanding the set of whitespace beyond what
     // Emacs will look for, but that Emacs function predates Unicode.  So this
-    // is probably an improvement.  We could _just_ scan for the whitespace
-    // instead of making a new string, but this is fast enough for an
+    // is probably an improvement.  
+    //
+    // We could _just_ scan for the whitespace instead of making a new string,
+    // but Typescript doesn't offer a builtin (non-regex) "is this character
+    // considered whitespace" function, we don't want to impose a (wrong)
+    // definition of what a "character" is, and this is fast enough for an
     // interactive keystroke.
     //
     const prefixTrimSize = prefix.length - prefix.trimEnd().length;
@@ -60,6 +88,22 @@ function fixupWhitespace()
       selection.active.translate(0, -prefixTrimSize),
       selection.active.translate(0, suffixTrimSize)
     );
+
+    // Form an indexing key for this erasure range based on its start position.
+    const key = `${erasure.start.line}:${erasure.start.character}`;
+
+    // Do we already know this range?  We do if we already indexed its start
+    // position.
+    if(already.has(key))
+    {
+      // Yes.  Skip it.
+      log(`Already have ${key}`);
+      continue;
+    }
+
+    // Add it to our index of regions we're planning to erase.
+    log(`Found region at ${key}`);
+    already.add(key);
 
     // We replace that erasure region put that back together with a single space
     // character, unless the cursor is at the start or end of the line, or we
@@ -74,32 +118,37 @@ function fixupWhitespace()
     edits.push({erasure, replacement});
   }
 
-  // TODO: Are selections always sorted?
-  //
-  // TODO: If not, can we sort them?
-
   // Perform all the edits at once by running all of the saved functions.
   editor.edit(
     edit => edits.forEach(args => {
-      console.log("TRY");
       edit.replace(args.erasure, args.replacement);
-      console.log("AFTER");
     })
   )
   .then(
+    // onFullfilled.
     success => {
-      // onFullfilled.
+      // Did it work?
       if(!success)
       {
-        // Assume it's because of overlapping regions.
+        // No.  Just complain about it generically, since that's all we know.
         vscode.window.showWarningMessage("Edit failed");
+        return;
       }
+
+      // Yes.  Consider whether we need to adjust the cursor.
+      //
+      // The cursor at _least_ at the start of the erasure range that we just
+      // replaces, so the TextEditor adjusted its position for us to the end of
+      // the replacement text, which is either empty or a space.  We want to be
+      // at the _beginning_ of that replacement text, so if the replacement is
+      // of non-zero size, move it back one space.
+      //
+      // TODO:
     },
+
     // onRejected.
     reason => {
       // Assume it's because of overlapping regions.
-      //
-      // TODO: Be smart enough to allow this.
       vscode.window.showWarningMessage(
         // The user can see our extension identity if they want to, but we
         // repeat it here so it's clearer what is going on.
@@ -129,7 +178,7 @@ function fixupWhitespace()
 // Activation hook.
 export function activate(context: vscode.ExtensionContext) 
 {
-  console.log("HELLO THERE");
+  log("HELLO THERE");
 
   // Define the command mappings.
 	let disposable = vscode.commands.registerCommand(
